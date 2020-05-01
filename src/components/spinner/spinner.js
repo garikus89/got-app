262256112cb8872050d6cec794801440a1c74984
Igor Mirkin <garikus89@yandex.ru>
2020-05-01 15:43:01 +0300

    created app

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-spinner-ie4kqttst7"><div className="ldio-c39adjzs4g">
+        <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+        </div></div>
+    )
+}
+
+export default Spinner;
